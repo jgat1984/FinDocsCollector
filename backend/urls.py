874fc6eedacfe72233eac20d1403e432b7cdd7f3
jgat1984 <jgat1984@yahoo.com>
2025-08-07 06:51:03 -0400

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('collectors.urls')),  # ✅ Routes all API calls to collectors
-    re_path(r'^.*$', TemplateView.as_view(template_name="index.html")),  # React app entry
+    #path('api/', include('collectors.urls')),  # ✅ Routes all API calls to collectors
+    path('api/company/<str:ticker>', company_data_view),  # ✅ API first
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),  # ✅ React fallback LAST
 ]
