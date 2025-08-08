e203183d39c9593d8b610aa7583c9292fb42083d
jgat1984 <jgat1984@yahoo.com>
2025-08-08 02:41:33 -0400

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.views.generic import TemplateView
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('collectors.urls')),
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
+    path('api/', include('collectors.urls')),  # Maps /api/* to collectors app
 ]
