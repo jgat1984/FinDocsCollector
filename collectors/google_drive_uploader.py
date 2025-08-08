eaa76681b637cb6fe07155a1b6d3691beac53e3b
jgat1984 <jgat1984@yahoo.com>
2025-08-07 23:17:15 -0400

    Update google_drive_uploader.py

--- collectors/google_drive_uploader.py
+++ collectors/google_drive_uploader.py
@@ -14,7 +14,7 @@ def upload_file_to_drive(file_path, folder_id):
     if os.path.exists('token.json'):
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
     else:
-        flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+        flow = InstalledAppFlow.from_client_secrets_file('../credentials.json', SCOPES)
         creds = flow.run_local_server(port=0)
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
